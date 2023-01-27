e8fa6dd28b038165de666d860c3eb64ebadf0ce6
developmentHF <development.henry@gmail.com>
2023-01-28 01:25:02 +1100

    initial project setup

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import time
+
+def intro():
+    # On script being launched, confirm if successful by greeting user
+    print("cherry pomodoro timer launch successful\nV.01")
+    print("-"*60)
+    print(f"When you are ready to begin the 25-minute working session, enter `begin`.\n\n")
+    return
+
+def workloop():
+    return
+
+if __name__ == "__main__":
+    begun = False
+    intro()
